874ce608be03827890920474b7be974fb768d93f
nightkuzan <aekkarit12@gmail.com>
2023-02-06 01:34:14 +0700

    create menu page and have back/side menu button

---
+++ lib/menu/menu.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:scheduletdl/todolist/listview.dart';
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Schedule',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Schedule'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Todolist()),
+                  );
+                },
+              ),
+              BackButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+          body: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Menu",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Todolist()),
+                  );
+                },
+                child: const Text('School'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Todolist()),
+                  );
+                },
+                child: const Text('Todo List'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
