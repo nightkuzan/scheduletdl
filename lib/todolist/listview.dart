cc23b6c2db537e6fdf356b51c80911790a0a71f4
nightkuzan <aekkarit12@gmail.com>
2023-02-06 01:47:57 +0700

    create TodoListPage Mockup

--- lib/todolist/listview.dart
+++ lib/todolist/listview.dart
@@ -8,8 +8,82 @@ class Todolist extends StatefulWidget {
 }
 
 class _TodolistState extends State<Todolist> {
+  List<dynamic> tasks = [
+    {
+      "taskname": "Math",
+      "taskdescription": "Do math homework",
+      "taskdate": "2021-10-10",
+      "tasktime": "10:00",
+      "taskpriority": "High",
+      "taskstatus": "Incomplete"
+    },
+    {
+      "taskname": "English",
+      "taskdescription": "Do english homework",
+      "taskdate": "2021-10-10",
+      "tasktime": "10:00",
+      "taskpriority": "High",
+      "taskstatus": "Incomplete"
+    }
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      // todolist app
+
+      title: 'Schedule',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Schedule'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Todolist()),
+                );
+              },
+            ),
+            BackButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Todo List",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            // create a listview to show the list of tasks
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(tasks[index]["taskname"]),
+                    subtitle: Text(tasks[index]["taskdescription"]),
+                    trailing: Text(tasks[index]["taskdate"]),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
