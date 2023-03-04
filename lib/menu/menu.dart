a719e10881735ed834ed16385135d811390751fe
sunnytulakorn <sunnytulakorn@gmail.com>
2023-03-05 00:41:54 +0700

    fix ui menu

--- lib/menu/menu.dart
+++ lib/menu/menu.dart
@@ -15,56 +15,76 @@ class _MenuState extends State<Menu> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Schedule'),
-          // actions: [
-          // IconButton(
-          //   icon: const Icon(Icons.menu),
-          //   onPressed: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(builder: (context) => const Todolist()),
-          //     );
-          //   },
-          // ),
-          // BackButton(
-          //   onPressed: () {
-          //     Navigator.pop(context);
-          //   },
-          // ),
-          // ],
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
+          Text(
+            "my",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          Text(
+            "Schedule",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Color(0xff6B4EFF)),
+          ),
+        ]),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new),
+          color: Colors.black,
         ),
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Menu",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const MenuSchedule()),
-                );
-              },
-              child: const Text('School'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Todolist()),
-                );
-              },
-              child: const Text('Todo List'),
-            ),
-          ],
+        backgroundColor: Colors.white,
+      ),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset('assets/images/pic_login.jpg'),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    backgroundColor:const Color(0xff8A2DE8)
+                  ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MenuSchedule()),
+                  );
+                },
+                child: const Text('School', style: TextStyle(fontSize: 24)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    backgroundColor:const Color(0xffB770FF)
+                  ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Todolist()),
+                  );
+                },
+                child: const Text('Todo List', style: TextStyle(fontSize: 24)),
+              ),
+            ],
+          ),
         ));
   }
 }
