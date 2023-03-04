afc68f9c470d867ab274bac8ff4e271b69e0d007
nightkuzan <aekkarit12@gmail.com>
2023-03-05 01:30:51 +0700

    add signout botton

--- lib/menu/menu.dart
+++ lib/menu/menu.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:scheduletdl/Reg-Sign/SignIn.dart';
 import 'package:scheduletdl/todolist/listview.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../firebase_options.dart';
@@ -47,6 +48,7 @@ class _MenuState extends State<Menu> {
           ]),
           leading: IconButton(
             onPressed: () {
+              FirebaseAuth.instance.signOut();
               Navigator.pop(context);
             },
             icon: const Icon(Icons.arrow_back_ios_new),
@@ -98,6 +100,26 @@ class _MenuState extends State<Menu> {
                   );
                 },
                 child: const Text('Todo List', style: TextStyle(fontSize: 24)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    backgroundColor: const Color.fromARGB(255, 105, 101, 110)),
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SignIn()),
+                  );
+                },
+                child: const Text('Log Out', style: TextStyle(fontSize: 24)),
               ),
             ],
           ),
