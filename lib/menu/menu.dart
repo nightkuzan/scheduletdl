b5af46740f4d0be28bdde10a5ea8df5ebef6e7e0
nightkuzan <aekkarit12@gmail.com>
2023-03-04 22:38:10 +0700

    add button and delete materialapp

--- lib/menu/menu.dart
+++ lib/menu/menu.dart
@@ -12,64 +12,58 @@ class Menu extends StatefulWidget {
 class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Schedule',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Schedule'),
+          // actions: [
+          // IconButton(
+          //   icon: const Icon(Icons.menu),
+          //   onPressed: () {
+          //     Navigator.push(
+          //       context,
+          //       MaterialPageRoute(builder: (context) => const Todolist()),
+          //     );
+          //   },
+          // ),
+          // BackButton(
+          //   onPressed: () {
+          //     Navigator.pop(context);
+          //   },
+          // ),
+          // ],
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Schedule'),
-            actions: [
-              // IconButton(
-              //   icon: const Icon(Icons.menu),
-              //   onPressed: () {
-              //     Navigator.push(
-              //       context,
-              //       MaterialPageRoute(builder: (context) => const Todolist()),
-              //     );
-              //   },
-              // ),
-              BackButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-          body: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                "Menu",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ScheduleManagement()),
-                  );
-                },
-                child: const Text('School'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Todolist()),
-                  );
-                },
-                child: const Text('Todo List'),
-              ),
-            ],
-          ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Menu",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ScheduleManagement()),
+                );
+              },
+              child: const Text('School'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Todolist()),
+                );
+              },
+              child: const Text('Todo List'),
+            ),
+          ],
         ));
   }
 }
