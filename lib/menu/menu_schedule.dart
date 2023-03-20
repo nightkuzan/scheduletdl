052ad121b2e56d1cf244e1bbac296d44c3dcc472
nightkuzan <aekkarit12@gmail.com>
2023-03-21 01:07:16 +0700

    add menu import from friend

--- lib/menu/menu_schedule.dart
+++ lib/menu/menu_schedule.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import '../Management/examDate_mng.dart';
 import '../sch-management/schedule-management.dart';
 import '../sch-view/schedule-view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MenuSchedule extends StatefulWidget {
   const MenuSchedule({super.key});
@@ -11,6 +15,43 @@ class MenuSchedule extends StatefulWidget {
 }
 
 class _MenuScheduleState extends State<MenuSchedule> {
+  User? user = FirebaseAuth.instance.currentUser;
+  List subjectList = [];
+  List uidList = [];
+  String uidimport = '';
+
+  initdata() async {
+    await Firebase.initializeApp();
+    final CollectionReference userstdl =
+        FirebaseFirestore.instance.collection('users');
+
+    final snapshot1 = await userstdl.get();
+    setState(() {
+      uidList = snapshot1.docs.map((e) => e.data()).toList();
+    });
+  }
+
+  getdataFromfriend(frienduid) async {
+    final CollectionReference taskschManagement = FirebaseFirestore.instance
+        .collection('users')
+        .doc(frienduid)
+        .collection('subjectList');
+
+    final snapshot = await taskschManagement.get();
+
+    setState(() {
+      subjectList = snapshot.docs.map((e) => e.data()).toList();
+      subjectList = subjectList[0]['subjectList'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initdata();
+    print(uidList);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,27 +153,81 @@ class _MenuScheduleState extends State<MenuSchedule> {
             const SizedBox(
               height: 20,
             ),
-            // Center(
-            //   child: ElevatedButton(
-            //     style: ElevatedButton.styleFrom(
-            //         minimumSize: const Size.fromHeight(60),
-            //         shape: RoundedRectangleBorder(
-            //           borderRadius: BorderRadius.circular(30.0),
-            //         ),
-            //         backgroundColor: const Color(0xff7B7B1B)),
-            //     onPressed: () {
-            //       Navigator.push(
-            //         context,
-            //         MaterialPageRoute(
-            //             builder: (context) => const ExamList_Management()),
-            //       );
-            //     },
-            //     child: const Text(
-            //       'Import From Friend',
-            //       style: TextStyle(fontSize: 24),
-            //     ),
-            //   ),
-            // ),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    backgroundColor: const Color(0xff7B7B1B)),
+                onPressed: () {
+                  //  show dialog for input uid of friend
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Import Schedule'),
+                        content: TextField(
+                          decoration: const InputDecoration(
+                            hintText: 'Enter UID',
+                          ),
+                          onChanged: (value) {
+                            uidimport = value;
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              // check uid is exist
+                              bool check = false;
+                              for (int i = 0; i < uidList.length; i++) {
+                                if (uidList[i]['uid'] == uidimport) {
+                                  check = true;
+                                  break;
+                                }
+                              }
+
+                              if (check) {
+                                await getdataFromfriend(uidimport);
+
+                                // print(subjectList);
+                                // remove all schedule
+                                FirebaseFirestore.instance
+                                    .collection('users')
+                                    .doc(user!.uid)
+                                    .collection('subjectList')
+                                    .doc('subjectTask')
+                                    .set({
+                                  'subjectList': subjectList,
+                                }, SetOptions(merge: true));
+                                Navigator.pop(context);
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const ScheduleManagement()));
+                              }
+                            },
+                            child: const Text('Import'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text(
+                  'Import From Friend',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
           ],
         ),
       ),
